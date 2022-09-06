5a4b387ff01ed933c374915580dca2888a398e9d
GwangA <ordin20118@gmail.com>
2022-09-06 12:55:02 +0900

    Init

---
+++ src/main/java/com/shellofmagic/web/ShellofmagicApplication.java
@@ -0,0 +1,13 @@
+package com.shellofmagic.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShellofmagicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShellofmagicApplication.class, args);
+	}
+
+}
