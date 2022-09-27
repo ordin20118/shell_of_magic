c353d640706d7044d092fee4879cc18c365b2635
ordin20118 <75382892+ordin20118@users.noreply.github.com>
2022-09-27 17:11:10 +0900

    Add share button.

--- src/main/resources/static/js/svc_ui_lib.js
+++ src/main/resources/static/js/svc_ui_lib.js
@@ -14,3 +14,14 @@ function moveToModify(answerId) {
 function moveToBack() {
 	
 }
+
+function shareFacebook() {
+    var sendUrl = "shellofmagic.com/"; // 전달할 URL
+    window.open("http://www.facebook.com/sharer/sharer.php?u=" + sendUrl);
+}
+
+function shareTwitter() {
+    var sendText = "고민을 말해보세요!"; // 전달할 텍스트
+    var sendUrl = "shellofmagic.com/"; // 전달할 URL
+    window.open("https://twitter.com/intent/tweet?text=" + sendText + "&url=" + sendUrl);
+}
