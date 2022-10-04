ada3e2b683de09cae5090fe2701bbe75d6c1a50f
ordin20118 <75382892+ordin20118@users.noreply.github.com>
2022-10-04 19:24:22 +0900

    ..

--- src/main/java/com/shellofmagic/web/controller/TestController.java
+++ src/main/java/com/shellofmagic/web/controller/TestController.java
@@ -1,14 +1,29 @@
 package com.shellofmagic.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.shellofmagic.web.dao.CategoryDto;
+import com.shellofmagic.web.service.ManageService;
 
 @Controller
 public class TestController {
 	
+	@Autowired
+	ManageService manageService;
+	
 	@GetMapping("/")
-	public String index() {		
-		return "redirect:page/home";
+	public String index(Model model) {
+
+		List<CategoryDto> allCategory = manageService.getAllCategory();
+		model.addAttribute("categoryList", allCategory);
+		
+		return "svc/index";
+		//return "redirect:page/home";
 	}
 
 }
